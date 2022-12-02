f127a89e00f1281780fa7dee7ecb334075420a0a
Ekaterina Garina <kvilcins@mail.ru>
2022-12-03 03:38:23 +0400

    adding new js statements

---
+++ styles/einstein.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const myWeight = 70;
+const speedLight = 3e8;
+const massEnergyEquivalence = myWeight * speedLight ** 2;
+console.log(speedLight);
+console.log(massEnergyEquivalence);
--- styles/script.js
+++ styles/script.js
@@ -1,9 +1,17 @@
 'use strict';
 
-const itemName = document.getElementById("name").value;
-const itemAmount = document.getElementById("amount").value;
-const itemCategory = document.getElementById("category").value;
-const itemPrice = document.getElementById("price").value;
-console.log(itemName)
-console.log((itemAmount * itemPrice) + ' ' + 'руб.')
+// const itemName = document.getElementById("name").value;
+// const itemAmount = document.getElementById("amount").value;
+// const itemCategory = document.getElementById("category").value;
+// const itemPrice = document.getElementById("price").value;
+// console.log(itemName);
+// console.log((itemAmount * itemPrice) + ' ' + 'руб.');
 
+const itemName = prompt("Наименование товара", '');
+const itemAmount = +prompt("Количество товара", '');
+const itemCategory = prompt("Категория товара", '');
+const itemPrice = +prompt("Цена товара", '');
+
+console.log(typeof itemAmount);
+console.log(typeof itemPrice);
+console.log(`На складе ${itemAmount} единицы товара "${itemName}" на сумму $${itemPrice * itemAmount}`);
